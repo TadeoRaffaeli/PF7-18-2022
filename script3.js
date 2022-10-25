156a9ea726cb90711eb5d60ec8f297580b13f0df
TadeoRaffaeli <tadeo.raffa@gmail.com>
2022-10-24 23:11:08 -0300

    working on JS

--- script3.js
+++ script3.js
@@ -23,7 +23,88 @@ let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0)
 let listaNombres = listaProductosConStock.map((prod) => prod.nombre)
 
 let precioTotal = 0
+//a eleiminar despues de la 2nda entrega
+listaNombres = ["Proteina ENA 1KG", "Proteina Star 3kg", "Whey Protein Gentech 5kgs", "Creatina ENA 300Grs", "Creatina Star 300Grs", "Creatina Gold Nutrition 300Grs"]
 
+alert('Estos son nuestros productos: \n - ' + listaNombres.join("\n - "))
+
+
+//a eleiminar despues de la 2nda entrega
+function calculoPrecio(cantidad, precio){
+    precioTotal +=  (cantidad * precio)
+}
+
+function calculoStock(cantidad, stock, precio){
+    if(cantidad <= stock){
+        calculoPrecio(cantidad, precio)}
+    else{
+    alert("Actualmente tenemos un stock total de " + stock)
+    }
+}
+
+let cantidadCompra = parseInt(prompt('Qué cantidad de productos distintos quiere comprar?: '))
+
+
+for(let i = 0; i  < cantidadCompra; i = i + 1){
+
+    let productoCompra = prompt("Ingrese que producto quiere comprar: \n- 1.Proteina ENA 1KG\n- 2.Proteina Star 3kg\n- 3.Whey Protein Gentech 5kg\n- 4.Creatina ENA 300Grs\n- 5.Creatina Star 300Grs\n- 6.Creatina Gold Nutrition 300Grs")
+
+
+    if((productoCompra.toLowerCase() == 'proteina ena 1kg') || (productoCompra == '1') || (productoCompra.toLowerCase() == '1.proteina ena 1kg')){
+        let cantidadProductoA = prompt("ingrese que cantidad de " + productoA.nombre + " desea comprar.")
+        calculoStock(cantidadProductoA, productoA.stock, productoA["precio"])
+        productoA.restarStock(cantidadProductoA)
+    }
+    else if((productoCompra.toLowerCase() == 'proteina star 3kg') || (productoCompra == '2') || (productoCompra.toLowerCase() =='2.proteina start 3kg')){
+     let cantidadProductoB = prompt("ingrese que cantidad de " + productoB.nombre + " desea comprar.")
+     calculoStock(cantidadProductoB, productoB.stock, productoB["precio"])
+     productoB.restarStock(cantidadProductoB)
+    }
+    else if((productoCompra.toLowerCase() == 'whey protein gentech 5kg') || (productoCompra == '3') || (productoCompra.toLowerCase() == '3.whey protein gentech 5kg')){
+     let cantidadProductoC = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar.")
+     calculoStock(cantidadProductoC, productoC.stock, productoC["precio"])
+     productoC.restarStock(cantidadProductoC)
+    }
+    else if((productoCompra.toLowerCase() == 'creatina ena 300grs') || (productoCompra == '4') || (productoCompra.toLowerCase() == '4.creatina ena 300grs')){
+        let cantidadProductoD = prompt("ingrese que cantidad de " + productoD.nombre + " desea comprar.")
+        calculoStock(cantidadProductoD, productoD.stock, productoD["precio"])
+        productoD.restarStock(cantidadProductoD)
+    }
+    else if((productoCompra.toLowerCase() == 'creatina star 300grs') || (productoCompra == '5') || (productoCompra.toLowerCase() == '5.creatina star 300grs')){
+        let cantidadProductoE = prompt("ingrese que cantidad de " + productoE.nombre + " desea comprar.")
+        calculoStock(cantidadProductoE, productoE.stock, productoE["precio"])
+        productoE.restarStock(cantidadProductoE)
+    }
+    else if((productoCompra.toLowerCase() == 'creatina gold nutrition 300grs') || (productoCompra == '6') || (productoCompra.toLowerCase() == '6.creatina gold nutrition 300grs')){
+        let cantidadProductoF = prompt("ingrese que cantidad de " + productoF.nombre + " desea comprar.")
+        calculoStock(cantidadProductoF, productoF.stock, productoF["precio"])
+        productoF.restarStock(cantidadProductoF)
+    }
+
+    else{
+     alert("No tenemos ese producto a la venta")
+    }
+    }
+    if(precioTotal != 0){
+        alert("El total de su compra será de: " + precioTotal)
+         if(precioTotal >= 210000){
+            alert('Su compra obtendrá un descuento de 30% ya que superó el monto de $210.000\n Su nuevo precio final será ' + precioTotal*0.70)
+         } 
+         else if(precioTotal >= 160000){
+        alert('Su compra obtendrá un descuento de 20% ya que superó el monto de $160.000\n Su nuevo precio final será ' + precioTotal*0.80)
+        } 
+        else if(precioTotal >= 90000){
+            alert('Su compra obtendrá un descuento de 10% ya que superó el monto de $90.000\n Su nuevo precio final será ' + precioTotal*0.90)
+        }
+        else if(precioTotal <12500){
+            alert('Para alcanzar el envío gratis debe alcanzar el monto mínimo de $12.500, su monto actual es ' + precioTotal + '\n El adicional de envío en CABA es de $1.100, su precio total será de: ' + (precioTotal+1100))
+        }
+    }
+    else{
+        alert("Muhcas gracias, vuelva pronto!")
+    }
+
+//De acá en adelante es lo que se conservará
 let catalogo = document.getElementById("catalogo")
 
 function render(lista){
@@ -64,18 +145,4 @@ let botonTodos = document.getElementById("todos")
 botonTodos.addEventListener("click", ()=>{render(listaProductosConStock)})
 
 
-
-/* console.log(catalogo)
-
-let card = document.getElementsByClassName("NombreDeLaClass")
-
-card[0].className = "nuevaCard" */
-
-/* catalogo.innerText = "No es bienvenido"
-
-console.log(catalogo.innerText)
-
-let catalogo = document.getElementById("nombreDelID")
-
-catalogo.innerHTML = "<h2>Hola</h2>" */
 
--- script4.js
+++ script4.js
@@ -1,3 +1,193 @@
-let iframe = document.getElementById('frameTest')
+const listaDeProductos = [
+    {
+        id: 1,
+        nombre: "Proteina ENA 1KG",
+        precio: 5500,
+        stock: 150,
+        img: "../Images/ProteinaENA1KG.jpg",
+        cat: "proteina"
+    },
+    {
+        id: 2,
+        nombre: "Proteina Star 3kg",
+        precio: 15200,
+        stock: 60,
+        img: "../Images/ProteinaStar3kg.jpg",
+        cat: "proteina"
+    },
+    {
+        id: 3,
+        nombre: "Whey Protein Gentech 5kgs",
+        precio: 24000,
+        stock: 20,
+        img: "../Images/WheyProteinGentech5kgs.jpg",
+        cat: "proteina"
+    },
+    {
+        id: 4,
+        nombre: "Creatina ENA 300Grs",
+        precio: 7200,
+        stock: 30,
+        img: "../Images/CreatinaENA300Grs.jpg",
+        cat: "creatina"
+    },
+    {
+        id: 5,
+        nombre: "Creatina Star 300Grs",
+        precio: 10000,
+        stock: 20,
+        img: "../Images/CreatinaStar300Grs.jpg",
+        cat: "creatina"
+    },
+    {
+        id: 6,
+        nombre: "Creatina Gold Nutrition 300Grs",
+        precio: 7000,
+        stock: 80,
+        img: "../Images/CreatinaGoldNutrition300Grs.jpg",
+        cat: "creatina"
+    }
+]
 
-iframe.innerHTML =  '<iframe class="ultimo"  src="https://www.youtube.com/embed/OPEDjl88P-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>'
+let catalog = document.getElementById('items')
+let cartList = document.getElementById('carrito')
+let buttonEmpty = document.getElementById('boton-vaciar')
+let totalValue = document.getElementById('total')
+let totalEnvio = document.getElementById('envio')
+let cart = []
+
+buttonEmpty.addEventListener('click', emptyButtonHandler)
+
+loadCartFromStorage()
+renderCart()
+
+
+listaDeProductos.forEach((prod) => {
+    let container = document.createElement('div')
+    container.classList.add('card', 'col-sm-4')
+    //Body
+    let cardBody = document.createElement("div")
+    cardBody.classList.add('card-body')
+    //Title
+    let cardTitle = document.createElement("h5")
+    cardTitle.classList.add('card-title')
+    cardTitle.innerText = prod.nombre
+    //Precio
+    let cardPrice = document.createElement("p")
+    cardPrice.classList.add('card-text')
+    cardPrice.innerText = `$${prod.precio}`
+    //Stock
+    let cardStock = document.createElement("p")
+    cardStock.classList.add('card-text')
+    cardStock.innerText = `Stock: ${prod.stock}`
+    //Img
+    let cardImg = document.createElement("img")
+    cardImg.classList.add('card-img')
+    cardImg.innerHTML = `<img src=${prod.img}></img>`
+    //Button
+    let cardButton = document.createElement("button")
+    cardButton.classList.add('btn', 'btn-primary')
+    cardButton.innerText = `Comprar`
+    cardButton.setAttribute('mark', prod.id)
+    cardButton.addEventListener('click', addProdToCart)
+
+    cardBody.append(cardTitle)
+    cardBody.append(cardPrice)
+    cardBody.append(cardImg)
+    cardBody.append(cardStock)
+    cardBody.append(cardButton)
+    container.append(cardBody)
+    catalog.append(container)
+})
+
+function addProdToCart(event){
+    cart.push(event.target.getAttribute('mark'))
+    renderCart()
+}
+
+function renderCart(){
+
+    saveCartToStorage()
+
+    cartList.innerHTML = ''
+
+    let cartWithoutRepeatedElements = [...new Set(cart)]
+
+    cartWithoutRepeatedElements.forEach((itemId) => {
+        let item = listaDeProductos.filter((producto) => {
+            return producto.id === parseInt(itemId)
+        })
+        let quantity = cart.reduce((total, id) => {
+            return id === itemId ? total += 1 : total
+        }, 0)
+    
+
+    let linea = document.createElement('li')
+    linea.classList.add('list-group-item', 'text-right', 'mx-2')
+    linea.innerText = `${quantity} x ${item[0].nombre} - $${item[0].precio}`
+
+    let buttonDelete = document.createElement('button')
+    buttonDelete.classList.add('btn', 'btn-danger', 'mx-5')
+    buttonDelete.innerText = 'X'
+    buttonDelete.dataset.item = itemId
+    buttonDelete.addEventListener('click', deleteProduct)
+
+    linea.append(buttonDelete)
+    cartList.append(linea)
+    })
+
+    totalValue.innerText = calculateTotalPrice()
+    totalEnvio.innerText = 1500
+}
+
+function deleteProduct(event){
+    
+ Swal.fire({
+    title: 'Querés eliminar este producto?',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: 'Eliminar',
+    denyButtonText: `Conservar producto`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+        let id = event.target.dataset.item
+    cart = cart.filter((cartId) => {
+       return cartId != id
+    })
+    renderCart()
+      Swal.fire('Guardado!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('Se conservó el producto', '', 'error')
+    }
+  })
+}
+
+function emptyButtonHandler(){
+    cart = []
+    cartList.innerHTML = ''
+    totalValue.innerText = 0
+}
+
+function calculateTotalPrice(){
+        return cart.reduce((total, itemId) => {
+        let item = listaDeProductos.filter((producto) => {
+            return producto.id === parseInt(itemId)
+        })
+        return total + item[0].precio
+     }, 0)   
+       
+}
+
+
+
+function saveCartToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+function loadCartFromStorage(){
+    if(localStorage.getItem('cart') !== null){
+        cart = JSON.parse(localStorage.getItem('cart'))
+    }
+}
+
